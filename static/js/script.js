8490af362b54588341f924545cfcabddc0f796fe
Mdsomratakbor <somrat876@gmail.com>
2020-10-24 19:23:26 +0600

    challenge two is complete

--- static/js/script.js
+++ static/js/script.js
@@ -13,3 +13,12 @@ document.getElementById('flex-box-result').appendChild(h1);
 function reset(){
     document.getElementById('ageInDays').remove();
 }
+
+
+// Challange 2: Cat Generator
+function GenerateCat(){
+    let image = document.createElement('img');
+    let div = document.getElementById('flex-cat-gen');
+    image.src="http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
+}
