4337b152b698c157e1dfb78497c0375e4150eb5f
Mdsomratakbor <somrat876@gmail.com>
2020-10-26 19:32:25 +0600

    Challenge 3 is half complete

--- static/js/script.js
+++ static/js/script.js
@@ -26,9 +26,44 @@ function GenerateCat(){
 // Chllange 3: Rock, Paper, Scissor
 
 function rpsGame(yourChoice){
-console.log(yourChoice.id)
+//console.log(yourChoice.id)
  let humenChoice, botChoice;
- //let results = decideWinner(humenChoice, botChoice);
- // message = finalMessage(results);
- rpsFrontEnd(yourChoice.id, botChoice, message);
+  humenChoice = yourChoice.id;
+  botChoice = numberToChoise(randomRPSToInt());
+  console.log('computerchoice',botChoice);
+ let results = decideWinner(humenChoice, botChoice);
+ console.log(results)
+  message = finalMessage(results);
+  console.log(message)
+ //rpsFrontEnd(yourChoice.id, botChoice, message);
+}
+
+function randomRPSToInt(){
+    return Math.floor(Math.random()*3);
+}
+
+function numberToChoise(number){
+    return ['rock', 'paper', 'scissors'][number];
+}
+function decideWinner(yourChoice, computerChoice){
+    let rpsDatabase = {
+        'rock':{'scissors':1,'rock':0.5, 'paper':0},
+        'paper':{'rock':1, 'paper':0.5, 'scissors':0},
+        'scissors':{'paper':1, 'scissors':0.5, 'rock':0},   
+    }
+    let yourScore = rpsDatabase[yourChoice][computerChoice];
+    let computerScore = rpsDatabase[computerChoice][yourChoice];
+    return [yourScore, computerScore];
+}
+
+function finalMessage([yourScore, computerScore]){
+if(yourScore === 0){
+return {'message' :'Your lost!', 'color':'red'};
+}
+else if(yourScore === 0.5){
+    return {'message' :'You tied!', 'color':'yellow'};
+}
+else{
+    return {'message':'You won!','color':'green'}
+}
 }
